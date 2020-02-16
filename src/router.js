a02bd90cbbc481ab5b186e6a273f80a75da149f6
Ankuryu <pmco89@gmail.com>
2020-02-16 11:08:30 +0530

    history mode enabled

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import About from './views/about'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
     routes: [
         {path:'/',name:'home',component:Home },
         {path:'/about',name:'about',component:About},
